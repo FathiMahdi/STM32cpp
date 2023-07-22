5ad8f8335890278a6a816f983df6439ce28027fd
FATHI MAHDI ELSIDDIG HAROUN <electronics_forever@outlook.com>
2023-07-23 04:05:15 +0800

    Adding vectors

--- Core/Src/Cppmain.cpp
+++ Core/Src/Cppmain.cpp
@@ -1,14 +1,31 @@
 #include "AMS.hpp"
 #include "main.h"
+#include <vector>
+
+using namespace std;
 
 
+vector <uint32_t> v;
+
+uint32_t time = 0;
 CppClass myCppClass;
 
 
 void CppMain()
 {
+
   while (1)
   {
-    myCppClass.ToggleLed();
+    myCppClass.ToggleLed(time);
+    if (time<5000)
+    {
+      time+100;
+    }
+    else
+    {
+      time=0;
+    }
+    
+    v.push_back(time); // increase the toggling time
   }
 }
